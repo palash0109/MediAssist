e3a729b96104d53cb0c39e92c89a9735b040c6ee
HP <avinashtiwary6300@gmail.com>
2020-11-21 16:50:51 +0530

    added all files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react'
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Login from './Component/Login'
+import { Container, Col, Row } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar1 from './Component/Navbar'
 import './App.css';
+import Signup from './Component/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+    <Router>
+      <Navbar1/>
+
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        <div>
+        </div>
+        <Route exact path='/' component={Login}></Route>
+        <Route path='/signup' component={Signup} ></Route>
+      </div>
+
+    </Router>
   );
 }
 
